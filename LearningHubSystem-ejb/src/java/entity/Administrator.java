d1e976eb81b6f969c15f74072d47441cbb97a064
linxianying <soullxy@qq.com>
2018-03-05 16:51:15 +0800

    test

--- LearningHubSystem-ejb/src/java/entity/Administrator.java
+++ LearningHubSystem-ejb/src/java/entity/Administrator.java
@@ -30,7 +30,7 @@ public class Administrator implements Serializable {
     private String email;
     
     @Column(length = 13, nullable = false)
-    private String telephone;
+    private String telephone; 
     
     @Column(length = 32, nullable = false, unique = true)
     private String username;
