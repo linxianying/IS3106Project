5185922a85a6da06b3f529018334bcd644d49cac
xuhong98 <xuhong9804@gmail.com>
2018-04-21 16:00:56 +0800

    merge

--- LearningHubSystem-rest/src/java/ws/restful/FileResource.java
+++ LearningHubSystem-rest/src/java/ws/restful/FileResource.java
@@ -5,11 +5,16 @@
  */
 package ws.restful;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.UUID;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.core.MediaType;
@@ -25,29 +30,54 @@ public class FileResource {
     @Context
     private UriInfo context;
 
-    /**
-     * Creates a new instance of FileResource
-     */
     public FileResource() {
     }
 
-    /**
-     * Retrieves representation of an instance of ws.restful.FileResource
-     * @return an instance of java.lang.String
-     */
-    @GET
-    @Produces(MediaType.APPLICATION_XML)
-    public String getXml() {
-        //TODO return proper representation object
-        throw new UnsupportedOperationException();
-    }
+//    @POST
+//    @Path("uploadImage")
+//    @Consumes(MediaType.MULTIPART_FORM_DATA)
+//    @Produces(MediaType.APPLICATION_XML)
+//    public void uploadImage(
+//            @FormDataParam("fileReference") String fileReference,
+//            @FormDataParam("caption") String caption,
+//            @FormDataParam("file") InputStream uploadedFileInputStream,
+//            @FormDataParam("file") FormDataContentDisposition uploadedFileDetails) {
+//        try {
+//            if (fileReference != null && fileReference.trim().length()) {
+//                Long fileReferenceLong = Long.parseLong(fileReference);
+//
+//                if (uploadedFileDetails.getSize() <= 2097152) {
+//                    String[] fileNameParts = uploadedFileDetails.getFileName().split("\\.");
+//
+//                    if ((fileNameParts[1].equals("jpg"))
+//                            || (fileNameParts[1].equals("jpeg")) || (fileNameParts[1].equals("gif"))
+//                            || (fileNameParts[1].equals("png"))) {
+//                        String newFilename = UUID.randomUUID().toString();
+//
+//                        File outputFile = new File("C:/" + newFilename + "." + fileNameParts[1]);
+//                        FileOutputStream out = new FileOutputStream(outputFile);
+//
+//                        int a;
+//                        int BUFFER_SIZE = 8192;
+//                        byte[] buffer = new byte[BUFFER_SIZE];
+//
+//                        while (true) {
+//                            a = uploadedFileInputStream.read(buffer);
+//                            if (a < 0) {
+//                                break;
+//                            }
+//                            out.write(buffer, 0, a);
+//                            out.flush();
+//                        }
+//
+//                        out.close();
+//                        uploadedFileInputStream.close();
+//                    }
+//                }
+//            }
+//        } catch (Exception ex) {
+//            ex.printStackTrace();
+//        }
+//    }
 
-    /**
-     * PUT method for updating or creating an instance of FileResource
-     * @param content representation for the resource
-     */
-    @PUT
-    @Consumes(MediaType.APPLICATION_XML)
-    public void putXml(String content) {
-    }
 }
